c176489d782b843f011a2c487f11e284fb051d02
ymh1995s <2014038117ab@gmail.com>
2025-01-13 17:50:34 +0900

    몬스터 객체지향 작업 전 준비

--- Assets/02_Scripts/ImMinhyuk/Monster/M_Eye.cs
+++ Assets/02_Scripts/ImMinhyuk/Monster/M_Eye.cs
@@ -1,16 +1,6 @@
 using UnityEngine;
 
-public class Eye : MonoBehaviour
+public class M_Eye : Monster
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
