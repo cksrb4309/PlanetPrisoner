072e739453fd1f2ba165e478b5f3e52a8397c3d7
cksrb4309 <rlacksrb0856@gmail.com>
2025-01-14 15:26:06 +0900

    Update PlayerAnimator
    Edit Scripts:
    PlayerAnimator

--- Assets/02_Scripts/KimChanGyu/Player/PlayerAnimator.cs
+++ Assets/02_Scripts/KimChanGyu/Player/PlayerAnimator.cs
@@ -4,27 +4,22 @@ public class PlayerAnimator : MonoBehaviour
 {
     Animator playerAnimator; // 플레이어 애니메이터
 
-    PlayerAnimationState currState; // 현재 상태
+    bool isGround = true; // 현재 isGround 상태
 
-    bool isGround = true;
     private void Start()
     {
         // Animator 가져오기
         playerAnimator = GetComponentInChildren<Animator>();
     }
-    public PlayerAnimationState GetState() // 현재 애니메이션 상태 반환
-    {
-        return currState;
-    }
-    void SetMoveSpeed(float value)
+    void SetMoveSpeed(float value) // 이동속도 값 적용
     {
         playerAnimator.SetFloat("Move", value);
     }
-    public void SetIsCrouch(bool isCrouch)
+    public void SetIsCrouch(bool isCrouch) // 앉은 상태 여부 적용
     {
         playerAnimator.SetBool("IsCrouch", isCrouch);
     }
-    public bool SetJumpTrigger()
+    public bool SetJumpTrigger() // 점프 애님이 불가능하면 False, 가능하면 True 반환 후 애니메이션 적용
     {
         // 현재 쭈구리고 있을 때
         if (playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("CrouchMove"))
@@ -38,27 +33,25 @@ public class PlayerAnimator : MonoBehaviour
         // true를 반환
         return true;
     }
-    public void SetIsGround(bool isGround)
+    public void SetIsGround(bool isGround) // IsGround 여부 적용
     {
+        // 현재 상태와 동일하다면 반환
         if (this.isGround == isGround) return;
 
+        // 현재 상태에 적용
         this.isGround = isGround;
 
+        // IsGround 적용
         playerAnimator.SetBool("IsGround", isGround);
     }
     private void OnEnable()
     {
+        // 함수 구독 시켜놓기
         GetComponent<PlayerController>().BindToPlayerAnimator(SetMoveSpeed);
     }
     private void OnDisable()
     {
+        // 함수 구독 해제하기
         GetComponent<PlayerController>().UnbindFromPlayerAnimator(SetMoveSpeed);
     }
-}
-
-public enum PlayerAnimationState
-{
-    Stand,
-    Crouch,
-    Jump
 }
