a665982db4068f522c5c4c0009955b33b44c1aa5
cksrb4309 <rlacksrb0856@gmail.com>
2025-02-04 00:15:16 +0900

    Update MainGameScene

--- Assets/02_Scripts/KimChanGyu/Player/PlayerGroundChecker.cs
+++ Assets/02_Scripts/KimChanGyu/Player/PlayerGroundChecker.cs
@@ -1,16 +1,36 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerGroundChecker : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public LayerMask groundLayerMask;
 
-    // Update is called once per frame
-    void Update()
+    public Transform topPosition;
+    public bool IsGround => isGround;
+    public bool IsColliderBelow => isColliderBelow;
+    [SerializeField] float range;
+
+    bool isTriggered = false;
+    bool isGround = false;
+    bool isColliderBelow = false;
+    Ray ray = new Ray();
+
+    private void Start()
     {
-        
+        ray.direction = Vector3.down;
+    }
+    private void Update()
+    {
+        ray.origin = topPosition.position;
+
+        isGround = isTriggered ? false : Physics.Raycast(ray, range, groundLayerMask);
+        isColliderBelow = Physics.Raycast(ray, range);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        isTriggered = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        isTriggered = false;
     }
 }
